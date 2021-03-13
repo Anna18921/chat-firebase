4862f165beca37c53bf9cd290a792a0134cdc6ba
rafaela <rafabispo53@gmail.com>
2021-03-13 12:43:12 -0300

    create RegisterActivity

--- app/src/main/java/com/example/chatfirebase2021/MainActivity.java
+++ app/src/main/java/com/example/chatfirebase2021/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.chatfirebase2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+ private EditText email;
+ private EditText password;
+ private Button button;
+ private TextView account;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.edit_email);
+        password = findViewById(R.id.edit_password);
+        account = findViewById(R.id.textview_signup);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String edit_email = email.getText().toString();
+                String edit_password = password.getText().toString();
+
+                Log.i("Teste", edit_email);
+                Log.i("Teste", edit_password);
+
+            }
+        });
+
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
